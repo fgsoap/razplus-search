c5bd7a96602337a96483b3d3ed4b9ec0f63b5d11
Scott Xu <fgsoap@gmail.com>
2022-09-18 15:51:07 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use scraper::{Html, Selector};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -18,7 +19,33 @@ async fn main() -> Result<(), reqwest::Error> {
 
     let res = reqwest::get(url).await?;
     let body = res.text().await?;
-    println!("{}", body);
+
+    let document = Html::parse_document(&body);
+    let top_selector = Selector::parse(r#"div.info>h2>a"#).unwrap();
+    let inner_elector = Selector::parse(r#"div.info>div.category"#).unwrap();
+
+    for (top_index, top_element) in document.select(&top_selector).enumerate() {
+        for (inner_index, inner_element) in document.select(&inner_elector).enumerate() {
+            if top_index == inner_index {
+                println!(
+                    "{:?}\n\n",
+                    top_element
+                        .html()
+                        .split("\n                            ")
+                        .collect::<Vec<_>>()[0..3]
+                        .iter()
+                        .cloned()
+                        .collect::<String>()
+                        .replace("a href=\"/books/leveled-books/book/?", "")
+                        .replace("amp;", "")
+                        .replace("  ", "")
+                        + inner_element.html().split(',').collect::<Vec<_>>()[3]
+                );
+            }
+        }
+    }
+
+    // println!("{}", body);
 
     Ok(())
 }
