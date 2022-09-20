8ca0debc2b8978eb5d3cd6f09da44089e3fdd943
Scott Xu <fgsoap@gmail.com>
2022-09-20 22:16:30 +0800

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,63 @@
+use clap::Parser;
+use scraper::{Html, Selector};
+use std::fmt::Write as _;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long, value_parser)]
+    name: String,
+}
+
+pub async fn run() -> Result<String, reqwest::Error> {
+    let name = Args::parse().name;
+    let url = format!(
+        "https://www.raz-plus.com/search/ajax-search.html?doSearch=Search&searchTerms={}",
+        name
+    );
+    eprintln!("Searching {:?}\n", url);
+
+    let res = reqwest::get(url).await?;
+    let body = res.text().await?;
+
+    let document = Html::parse_document(&body);
+    let top_selector = Selector::parse(r#"div.info>h2>a"#).unwrap();
+    let inner_elector = Selector::parse(r#"div.info>div.category"#).unwrap();
+
+    let mut result = String::from("");
+
+    for (top_index, top_element) in document.select(&top_selector).enumerate() {
+        for (inner_index, inner_element) in document.select(&inner_elector).enumerate() {
+            if top_index == inner_index
+                && top_element.html().contains("leveled-books")
+                && inner_element.html().contains("leveled-books")
+            {
+                if let Err(e) = writeln!(
+                    result,
+                    "{}{}",
+                    top_element
+                        .html()
+                        .split("\n                            ")
+                        .collect::<Vec<_>>()[0..3]
+                        .iter()
+                        .cloned()
+                        .collect::<String>()
+                        .replace("href=\"/books/leveled-books/book/?", "")
+                        .replace("amp;", "")
+                        .replace("  ", "")
+                        .replace("<strong>", "")
+                        .replace("</strong>", ""),
+                    &inner_element.html().split(" Level ").collect::<Vec<_>>()[1]
+                        .replace("                    </div>", "")
+                        .replace("<strong>", "")
+                        .replace("</strong>", "")
+                ) {
+                    eprintln!("{}", e);
+                    std::process::exit(1);
+                }
+            }
+        }
+    }
+
+    Ok(result)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,6 @@
-use clap::Parser;
-use scraper::{Html, Selector};
-
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    #[clap(short, long, value_parser)]
-    name: String,
-}
-
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let name = Args::parse().name;
-    let url = format!(
-        "https://www.raz-plus.com/search/ajax-search.html?doSearch=Search&searchTerms={}",
-        name
-    );
-    eprintln!("Searching {:?}\n", url);
-
-    let res = reqwest::get(url).await?;
-    let body = res.text().await?;
-
-    let document = Html::parse_document(&body);
-    let top_selector = Selector::parse(r#"div.info>h2>a"#).unwrap();
-    let inner_elector = Selector::parse(r#"div.info>div.category"#).unwrap();
-
-    for (top_index, top_element) in document.select(&top_selector).enumerate() {
-        for (inner_index, inner_element) in document.select(&inner_elector).enumerate() {
-            if top_index == inner_index
-                && top_element.html().contains("leveled-books")
-                && inner_element.html().contains("leveled-books")
-            {
-                println!(
-                    "{:#?}",
-                    top_element
-                        .html()
-                        .split("\n                            ")
-                        .collect::<Vec<_>>()[0..3]
-                        .iter()
-                        .cloned()
-                        .collect::<String>()
-                        .replace("href=\"/books/leveled-books/book/?", "")
-                        .replace("amp;", "")
-                        .replace("  ", "")
-                        .replace("<strong>", "")
-                        .replace("</strong>", "")
-                        + &inner_element.html().split(" Level ").collect::<Vec<_>>()[1]
-                            .replace("                    </div>", "")
-                            .replace("<strong>", "")
-                            .replace("</strong>", "")
-                );
-            }
-        }
-    }
-
-    // println!("{}", body);
-
+    let result = razplus_search::run().await?;
+    println!("{}", result);
     Ok(())
 }
